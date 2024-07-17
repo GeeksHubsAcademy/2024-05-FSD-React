0268b04b398b15606a0c7acfc6fee515ffbf9dea
datata <dani@danitarazona.dev>
2024-07-17 13:08:41 +0200

    feat: Login Component created

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+
+export const Login = () => {
+  const [credentials, setCredentials] = useState(
+    {
+      email: "",
+      password: ""
+    }
+  )
+
+
+  function handleChange(e) {
+    console.log('HandleChange');
+    setCredentials(prevState => (
+      {
+        ...prevState,
+        [e.target.name]: e.target.value
+      }
+    ))
+  }
+
+  function login() {
+    console.log('LOGIN');
+    console.log(credentials);
+  }
+
+
+
+
+  // OPCION 1
+	// const [email, setEmail] = useState("");
+	// const [password, setPassword] = useState("");
+
+	// function handleChange(e) {
+	// 	console.log("handleChange");
+
+	// 	if (e.target.name === "email") {
+	// 		setEmail(e.target.value);
+	// 	}
+
+	// 	if (e.target.name === "password") {
+	// 		setPassword(e.target.value);
+	// 	}
+	// }
+
+	// function login() {
+	// 	console.log("LOGIN");
+
+	// 	if (email.length === 0 || password.length === 0) {
+	// 		return alert("Email and password are required");
+	// 	}
+
+	// 	console.log(email);
+	// 	console.log(password);
+
+	// 	// fetch a la api /login
+	// }
+
+	return (
+		<>
+			<h1> Login </h1>
+			<div>
+				{/* <label htmlFor="email">Email:</label> */}
+				<input
+					type="email"
+					name="email"
+					placeholder="Email"
+					onChange={handleChange}
+				/>
+			</div>
+			<div>
+				{/* <label htmlFor="password">Password:</label> */}
+				<input
+					type="password"
+					name="password"
+					placeholder="Password"
+					onChange={handleChange}
+				/>
+			</div>
+			<input type="button" value="Login" onClick={login} />
+		</>
+	);
+}
