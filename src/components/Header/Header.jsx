456e4abc7a4a39c3cb899b2e5c6c9f5064f3155e
datata <dani@danitarazona.dev>
2024-07-18 12:18:04 +0200

    feat: CSurfer created and services view

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { CSurfer } from "../CSurfer/CSurfer";
 
 export const Header = () => {
+	const navigate = useNavigate();
+
 	return (
 		<>
-			<div className="header">Header</div>
+			<CSurfer path="/" content="Home"/>			
+			<CSurfer path="/services" content="Services"/>			
+			<div onClick={() => navigate("/login")}> Login </div>
+			<div> Register</div>
 		</>
 	);
 };
--- src/views/Body/Body.jsx
+++ src/views/Body/Body.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Login } from '../../components/Login/Login';
 import { Home } from '../Home/Home';
 import { NotFound } from '../NotFound/NotFound';
+import { Services } from '../Services/Services';
 
 export const Body = () => {
   return (
@@ -11,6 +12,7 @@ export const Body = () => {
         <Route path="*" element={<NotFound />}/>
         <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />}/>
+        <Route path="/services" element={<Services />}/>
       </Routes>
     </>
   )
